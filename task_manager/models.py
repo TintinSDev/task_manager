6b218c44a98cb703c4304b9a9ecd5757557216b3
Martin Maina <mmaina290@gmail.com>
2024-02-13 22:03:43 +0300

    Update models.py

--- task_manager/models.py
+++ task_manager/models.py
@@ -1,3 +1,6 @@
+import os
+import sys
+sys.path.append(os.getcwd)
 from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, create_engine
 from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
